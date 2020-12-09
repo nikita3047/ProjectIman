9fbf431fa8c07b6b33dcca6680b0741d668ae4ee
NIkita <nikita3047@gmail.com>
2020-12-09 11:01:09 +0300

    prefinal

--- routes/todos.js
+++ routes/todos.js
@@ -29,7 +29,7 @@ router.get('/', async (req, res) => {
   })
 
   res.render('index', {
-    title: 'Записи',
+    title: 'Студия красоты и здоровья ИМАН',
     isIndex: true,
     visitsToday,
     strtoday
